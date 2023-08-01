9705e73afc70932858e4635b0b3027c21f4d8dc0
ayushsnowflake18 <140854307+ayushsnowflake18@users.noreply.github.com>
2023-08-01 17:58:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import requests
 import pandas
 import snowflake.connector
-from urllib.error import URLERROR
+from urllib.error import URLError
 streamlit.title('My Moms New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
