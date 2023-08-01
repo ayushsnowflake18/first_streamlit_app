734353bc539a495d95bdf12916fb247669f728f9
ayushsnowflake18 <140854307+ayushsnowflake18@users.noreply.github.com>
2023-08-02 02:30:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,19 +17,19 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
+
+
+
+
+
+
+
+
 #create the repeatable code block (called function)
-def get_fruityvise_data(this_fruit_choice):
+def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvise_normalized
-
-
-
-
-
-
-
-
+    return fruityvice_normalized
 
 #New Section to display fruityvise api response
 streamlit.header("Fruityvice Fruit Advice!")
@@ -38,7 +38,7 @@ try:
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
     else:
-        back_from_function = get_fruityvise_data(fruit_choice)
+        back_from_function = get_fruityvice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
 
 except URLError as e:
